b48c65746c8e2dc736b7ef74b671746416f308d4
subotic123@ukr.net <subotic123@ukr.net>
2022-01-31 22:11:16 +0200

    Add StyleCop

---
+++ hw2/hw2/hw2/Cartcs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul2hw2
+{
+    public class Cartcs
+    {
+        private Product[] _products;
+        private int _value = 0;
+        public Cartcs()
+        {
+            _products = new Product[1];
+        }
+
+        public Product[] Add(ref Product[] produkt, ref int[] arr)
+        {
+            _products = new Product[arr.Length];
+            foreach (var item in arr)
+            {
+                for (var i = 0; i < produkt.Length; i++)
+                {
+                    if (item == i)
+                    {
+                        _products[_value++] = produkt[i];
+                    }
+                }
+            }
+
+            return _products;
+        }
+    }
+}
---
+++ hw2/hw2/hw2/ProductOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul2hw2
+{
+   public class ProductOrder
+    {
+        public void Order(Product[] products, string name, string secondName)
+        {
+            Console.WriteLine("----------------------------------------");
+            var random = new Random();
+            var price = 0;
+            Console.Write($"{name} {secondName},Your order: ");
+
+            foreach (var item in products)
+            {
+                Console.Write($"{item.Name},");
+            }
+
+            Console.WriteLine("In total:");
+            for (var i = 0; i < products.Length; i++)
+            {
+                price += products[i].Price;
+            }
+
+            Console.WriteLine(price);
+            Console.WriteLine($"Order number:{random.Next(1, 100)}");
+        }
+    }
+}
